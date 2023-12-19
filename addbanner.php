7b15afbbc9b59f0820aa9b38f90aa5e48e08491b
OdinAdema <144688155+OdinAdema@users.noreply.github.com>
2023-12-19 11:10:06 +0100

    Changed icon & added instructions database

--- addbanner.php
+++ addbanner.php
@@ -56,6 +56,35 @@ if(isset($_POST['Submit'])){
     }
 }
 
+/* ___Instructies___
+
+///Indien de database niet automatisch wordt geüpdatet, voer dan de volgende query uit in phpMyAdmin/MySQL workbench:
+
+CREATE TABLE `sliderimage` (
+  `ImagePath` varchar(255) NOT NULL,
+  `SliderID` int(11) NOT NULL AUTO_INCREMENT,
+  `StockItemID` int(11) NOT NULL,
+  `SliderName` varchar(255) NOT NULL,
+  PRIMARY KEY (`SliderID`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+///Dit creëert de tabel die nodig is voor de slider
+///Daarna kan je de slider gebruiken door naar addbanner.php te gaan en hier bestanden to te voegen. 
+///Standaard zullen er 3 sliders meegeleverd worden, om deze toe te voegen aan de database, voer de volgende queries uit:
+
+INSERT INTO `sliderimage` (`ImagePath`, `SliderID`, `StockItemID`, `SliderName`)
+VALUES
+	('Pub/Banner/Slider-1.png', 1, 16, 'slider1');
+
+ INSERT INTO `sliderimage` (`ImagePath`, `SliderID`, `StockItemID`, `SliderName`)
+VALUES
+	('Pub/Banner/Slider-2.png', 2, 34, 'slider2');
+
+INSERT INTO `sliderimage` (`ImagePath`, `SliderID`, `StockItemID`, `SliderName`)
+VALUES
+	('Pub/Banner/Slider-3.jpg', 3, 28, 'slider3');
+   
+*/
 
 include __DIR__ . "/components/footer.php";
 ?>
