31632c7cc531f81d1c8f9a32857fc796aa2ef594
Naweed2023 <luxnoori@gmail.com>
2023-12-22 13:38:59 +0100

    last edit

--- lib/database.php
+++ lib/database.php
@@ -340,3 +340,30 @@ function changeInventoryByOrderId($orderId, $dbconn){
     }
 
 }
+
+//Toenen van stockitemID quantity (Meest 3 verkochte producten).
+
+function meestVerkochtProduct($limit, $databaseConnection){
+    $Query = "
+        SELECT StockItemID, SUM(quantity) AS totalQuantity
+        FROM orderlines
+        GROUP BY StockItemID 
+        ORDER BY totalQuantity DESC
+        LIMIT ?;
+    ";
+
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_bind_param($Statement, "i", $limit);
+    mysqli_stmt_execute($Statement);
+    $Result = mysqli_stmt_get_result($Statement);
+
+    $meestVerkocht = [];
+
+    while ($row = mysqli_fetch_assoc($Result)) {
+        $meestVerkocht[] = $row;
+    }
+
+    mysqli_stmt_close($Statement);
+
+    return $meestVerkocht;
+}
--- view.php
+++ view.php
@@ -10,10 +10,10 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
 <div id="CenteredContent">
     <?php
     if ($StockItem != null) {
-        ?>
+    ?>
         <?php
         if (isset($StockItem['Video'])) {
-            ?>
+        ?>
             <div id="VideoFrame">
                 <?php print $StockItem['Video']; ?>
             </div>
@@ -26,10 +26,9 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
             if (isset($StockItemImage)) {
                 // één plaatje laten zien
                 if (count($StockItemImage) == 1) {
-                    ?>
-                    <div id="ImageFrame"
-                         style="background-image: url('Pub/StockItemIMG/<?php print $StockItemImage[0]['ImagePath']; ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;"></div>
-                    <?php
+            ?>
+                    <div id="ImageFrame" style="background-image: url('Pub/StockItemIMG/<?php print $StockItemImage[0]['ImagePath']; ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;"></div>
+                <?php
                 } else if (count($StockItemImage) >= 2) { ?>
                     <!-- meerdere plaatjes laten zien -->
                     <div id="ImageFrame">
@@ -37,17 +36,16 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
                             <!-- Indicators -->
                             <ul class="carousel-indicators">
                                 <?php for ($i = 0; $i < count($StockItemImage); $i++) {
-                                    ?>
-                                    <li data-target="#ImageCarousel"
-                                        data-slide-to="<?php print $i ?>" <?php print (($i == 0) ? 'class="active"' : ''); ?>></li>
-                                    <?php
+                                ?>
+                                    <li data-target="#ImageCarousel" data-slide-to="<?php print $i ?>" <?php print(($i == 0) ? 'class="active"' : ''); ?>></li>
+                                <?php
                                 } ?>
                             </ul>
 
                             <!-- slideshow -->
                             <div class="carousel-inner">
                                 <?php for ($i = 0; $i < count($StockItemImage); $i++) {
-                                    ?>
+                                ?>
                                     <div class="carousel-item <?php print ($i == 0) ? 'active' : ''; ?>">
                                         <img src="Pub/StockItemIMG/<?php print $StockItemImage[$i]['ImagePath'] ?>">
                                     </div>
@@ -63,13 +61,12 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
                             </a>
                         </div>
                     </div>
-                    <?php
+                <?php
                 }
             } else {
                 ?>
-                <div id="ImageFrame"
-                     style="background-image: url('Pub/StockGroupIMG/<?php print $StockItem['BackupImagePath']; ?>'); background-size: cover;"></div>
-                <?php
+                <div id="ImageFrame" style="background-image: url('Pub/StockGroupIMG/<?php print $StockItem['BackupImagePath']; ?>'); background-size: cover;"></div>
+            <?php
             }
             ?>
 
@@ -89,64 +86,109 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
                             <button class=" mt-4" type="submit" name="submit" value="Voeg toe aan winkelmandje">Voeg toe aan <i class="fa fa-shopping-cart"></i></button>
                         </form>
                         <?php
-                            if (isset($_POST["submit"])) {              // zelfafhandelend formulier
-                                $stockItemID = $_POST["stockItemID"];
-                                addProductToCart("$stockItemID");         // maak gebruik van geïmporteerde functie uit cartfuncties.php
-                                print("Product toegevoegd aan <a href='cart.php'> winkelmandje!</a>");
-                            }
+                        if (isset($_POST["submit"])) {              // zelfafhandelend formulier
+                            $stockItemID = $_POST["stockItemID"];
+                            addProductToCart("$stockItemID");         // maak gebruik van geïmporteerde functie uit cartfuncties.php
+                            print("Product toegevoegd aan <a href='cart.php'> winkelmandje!</a>");
+                        }
                         ?>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div id="StockItemDescription">
-            <h3>Artikel beschrijving</h3>
-            <p><?php print $StockItem['SearchDetails']; ?></p>
-        </div>
+
         <div id="StockItemSpecifications">
             <h3>Artikel specificaties</h3>
             <?php
             $CustomFields = json_decode($StockItem['CustomFields'], true);
             if (is_array($CustomFields)) { ?>
                 <table>
-                <thead>
-                <th>Naam</th>
-                <th>Data</th>
-                </thead>
-                <?php
-                foreach ($CustomFields as $SpecName => $SpecText) { ?>
-                    <tr>
-                        <td>
-                            <?php print $SpecName; ?>
-                        </td>
-                        <td>
-                            <?php
-                            if (is_array($SpecText)) {
-                                foreach ($SpecText as $SubText) {
-                                    print $SubText . " ";
+                    <thead>
+                        <th>Naam</th>
+                        <th>Data</th>
+                    </thead>
+                    <?php
+                    foreach ($CustomFields as $SpecName => $SpecText) { ?>
+                        <tr>
+                            <td>
+                                <?php print $SpecName; ?>
+                            </td>
+                            <td>
+                                <?php
+                                if (is_array($SpecText)) {
+                                    foreach ($SpecText as $SubText) {
+                                        print $SubText . " ";
+                                    }
+                                } else {
+                                    print $SpecText;
                                 }
-                            } else {
-                                print $SpecText;
-                            }
-                            ?>
-                        </td>
-                    </tr>
-                <?php } ?>
+                                ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
                 </table><?php
-            } else { ?>
+                    } else { ?>
 
                 <p><?php print $StockItem['CustomFields']; ?>.</p>
-                <?php
-            }
+            <?php
+                    }
             ?>
         </div>
-        <?php
-    } else {
-        ?><h2 id="ProductNotFound">Het opgevraagde product is niet gevonden.</h2><?php
-    } ?>
-</div>
+        <div id="StockItemDescription">
+            <h3>Artikel beschrijving</h3>
+            <p><?php print $StockItem['SearchDetails']; ?></p>
+        </div>
+</div><br><br><br><br>
+  
+<!-- Meest Gekocht Producten -->
+<h1 style="font-size: 24px; color: red;">Meest Gekocht producten</h1>
+<div class="MeestGekochteContainer">
+    <?php
+    $mostSoldProducts = meestVerkochtProduct(3, $databaseConnection);
 
-<?php 
+    foreach ($mostSoldProducts as $product) {
+        $productId = $product['StockItemID'];
+        $productInfo = getStockItem($productId, $databaseConnection);
+        $productImages = getStockItemImage($productId, $databaseConnection);
+
+        if ($productInfo) {
+            $shortenedName = substr($productInfo['StockItemName'], 0, 15) . '...';
+    ?>
+            <!-- Meest gekocht producten item -->
+            <div class="MeestGekochtProduct">
+                <!-- Product Image -->
+                <div class="ImageFrame" style="background-image: url('Pub/StockItemIMG/<?php print $productImages[0]['ImagePath']; ?>'); background-size: cover; background-repeat: no-repeat; background-position: center; height: 100px; width: 100px;"></div>
+
+                <!-- Product Information -->
+                <div class="ArticleHeader">
+                    <h4 class="StockItemID">Artikelnummer: <?php print $productInfo["StockItemID"]; ?></h4>
+                    <h4 class="StockItemNameViewSize1 StockItemName"><?php print $shortenedName; ?></h4>
+                    <p class="StockItemPriceText"><b><?php print sprintf("€ %.2f", $productInfo['SellPrice']); ?></b></p>
+                    <form method="post">
+                        <input type="number" name="stockItemID" value="<?php print $productId; ?>" hidden>
+                        <button class="mt-2" type="submit" name="submit" value="Voeg toe aan winkelmandje">Voeg toe aan <i class="fa fa-shopping-cart"></i></button>
+                    </form>
+                    <?php
+                    if (isset($_POST["submit"])) {
+                        $stockItemID = $_POST["stockItemID"];
+                        addProductToCart("$stockItemID");
+                        print("Product toegevoegd aan <a href='cart.php'> winkelmandje!</a>");
+                    }
+                    ?>
+                </div>
+            </div>
+    <?php
+        }
+    }
+    ?>
+</div>
+    <?php
+    } else {
+    ?><h2 id="ProductNotFound">Het opgevraagde product is niet gevonden.</h2><?php
+                                                                                } ?>
+
+
+<?php
 include __DIR__ . "/components/footer.php";
 ?>
